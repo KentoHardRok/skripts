520eecb3870648c30755949890a534a47ecdf578
tomw <tom.wilent@gmail.com>
2019-12-11 14:43:35 -0500

    Added day3 info

---
+++ advent/day3.py
@@ -0,0 +1,28 @@
+cable_path = "/wrk/skripts/advent/cable_path"
+
+# puts file in list
+with open(cable_path) as f:
+    cables = f.read().split("\n")
+# splits list into array with individual steps
+for i in range(len(cables)):
+    cables[i] = cables[i].split(",")
+
+for j in range(i):
+    print("Cable Num: ",j+1)
+    x = 0
+    y = 0
+    for k in range(len(cables[j])):
+        if cables[j][k][0] == "R":
+            x += int(cables[j][k][1:])
+        elif cables[j][k][0] == "L":
+            x -= int(cables[j][k][1:])
+        elif cables[j][k][0] == "U":
+            y += int(cables[j][k][1:])
+        elif cables[j][k][0] == "D":
+            y -= int(cables[j][k][1:])
+        print("Coord[",k,"]",x,",",y)    
+            
+
+print(len(cables[0]))
+print(i)
+print(x, y)
