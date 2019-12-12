0800a2719cf334253488cce1810d6d4038e25f1f
tomw <tom.wilent@gmail.com>
2019-12-12 14:01:37 -0500

    Added day3 info

--- advent/day3.py
+++ advent/day3.py
@@ -1,5 +1,7 @@
+
 cable_path = "/wrk/skripts/advent/cable_path"
-cable_coord = [dict(),dict()]
+cable_coord = [dict(), dict()]
+
 # puts file in list
 with open(cable_path) as f:
     cables = f.read().split("\n")
@@ -8,21 +10,38 @@ for i in range(len(cables)):
     cables[i] = cables[i].split(",")
 
 for j in range(i):
-    print("Cable Num: ",j+1)
     x = 0
     y = 0
+    nc = 0
     for k in range(len(cables[j])):
         if cables[j][k][0] == "R":
-            x += int(cables[j][k][1:])
+            for c in list(range(x, int(cables[j][k][1:]))):
+                x += 1
+                cable_coord[j][nc] = (x,y)
+                nc = nc+1
+            c=0
         elif cables[j][k][0] == "L":
-            x -= int(cables[j][k][1:])
+            for c in list(range(x, int(cables[j][k][1:]))):
+                x -= 1
+                cable_coord[j][nc] = (x,y)
+                nc += 1
+            c=0
         elif cables[j][k][0] == "U":
-            y += int(cables[j][k][1:])
-        elif cables[j][k][0] == "D":
-            y -= int(cables[j][k][1:])
-        cable_coord[j][k] = (x,y)    
-            
+            for c in list(range(y, int(cables[j][k][1:]))):
+                y += 1
+                cable_coord[j][nc] = (x,y)
+                nc += 1
+            c=0
 
-print(cable_coord[1][199][0])
-print(i)
-print(x, y)
+        elif cables[j][k][0] == "D":
+            for c in list(range(y, int(cables[j][k][1:]))):
+                y -= 1
+                cable_coord[j][nc] = (x,y)
+                nc += 1
+            c=0
+
+cable_list=list(cable_coord(0))
+print (cable_list)
+
+
+
